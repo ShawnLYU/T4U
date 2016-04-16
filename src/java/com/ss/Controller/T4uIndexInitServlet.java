2dcf3377fb2a9745e5b1cc7e3bddbf585b932035
maobowen <maobowen3@hotmail.com>
2016-04-16 15:29:42 +0800

    Updated Movie DAO
    1. Updated Movie DAO, changed to PreparedStatement;
    2. Created index initialization servlet for loading resources for
    index.jsp;
    3. Changed welcome page to index initialization servlet (index.do).

---
+++ src/java/com/ss/Controller/T4uIndexInitServlet.java
@@ -0,0 +1,87 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ss.Controller;
+
+import com.ss.DAO.T4uMovieDAO;
+import com.ss.Model.T4uMovie;
+import com.ss.app.T4uConstants;
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import org.apache.log4j.Logger;
+
+/**
+ *
+ * @author Steven
+ */
+public class T4uIndexInitServlet extends HttpServlet {
+
+    private static final Logger LOGGER = Logger.getLogger(T4uIndexInitServlet.class);
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        LOGGER.debug("Load resources for index.jsp.");
+        HttpSession session = request.getSession(true);
+        List<T4uMovie> allMovies = T4uMovieDAO.getAllMovies();
+        session.setAttribute(T4uConstants.T4U_ALLMOVIES, allMovies);
+        LOGGER.debug("Error: "+ session.getAttribute("error"));
+        RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- src/java/com/ss/DAO/T4uVersionDAO.java
+++ src/java/com/ss/DAO/T4uVersionDAO.java
@@ -18,6 +18,7 @@ import java.util.logging.Logger;
  * @author Steven Mao
  * 
  * 2016041201    SM    Implemented getting all versions.
+ * 2016041501    SM    Used PreparedStatement.
  */
 public class T4uVersionDAO {
     /**
@@ -26,27 +27,28 @@ public class T4uVersionDAO {
     * @param movie A Movie object.
     * @return      A list of Version objects which belong to this movie.
     */
-//    public List<T4uVersion> getAllVersions(T4uMovie movie) {
-//        int movieId = movie.getMovieId();
-//        List<T4uVersion> allVersions=new ArrayList<T4uVersion>();
-//        try {
-//            T4uJDBC db = new T4uJDBC();
-//            String sql = "SELECT * FROM [T4U_version] WHERE [MovieId]=" + movieId;
-//            ResultSet rs = db.query(sql);
-//            while (rs.next()) {
-//                T4uVersion version = new T4uVersion();
-//                version.setVersionId(rs.getInt("VersionId"));
-//                version.setMovieId(rs.getInt("MovieId"));
-//                version.setVersionName(rs.getNString("VersionName"));
-//                version.setVersionBasePrice(rs.getDouble("VersionBasePrice"));
-//                allVersions.add(version);
-//            }
-//            db.close(rs);
-//        } catch (SQLException ex) {
-//            Logger.getLogger(T4uMovieDAO.class.getName()).log(Level.SEVERE, null, ex);
-//        } catch (Exception ex) {
-//            Logger.getLogger(T4uMovieDAO.class.getName()).log(Level.SEVERE, null, ex);
-//        }
-//        return allVersions;
-//    }
+    public static List<T4uVersion> getAllVersions(T4uMovie movie) {
+        int movieId = movie.getMovieId();
+        List<T4uVersion> allVersions=new ArrayList<T4uVersion>();
+        try {
+            Connection conn =  T4uJDBC.connect();
+            PreparedStatement pstmt = conn.prepareStatement("SELECT * FROM [T4U_version] WHERE [MovieId]= ?");
+            pstmt.setInt(1, movieId);
+            ResultSet rs = pstmt.executeQuery();
+            while (rs.next()) {
+                T4uVersion version = new T4uVersion();
+                version.setVersionId(rs.getInt("VersionId"));
+                version.setMovieId(rs.getInt("MovieId"));
+                version.setVersionName(rs.getNString("VersionName"));
+                version.setVersionBasePrice(rs.getDouble("VersionBasePrice"));
+                allVersions.add(version);
+            }
+            T4uJDBC.close(rs, pstmt, conn);
+        } catch (SQLException ex) {
+            Logger.getLogger(T4uMovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (Exception ex) {
+            Logger.getLogger(T4uMovieDAO.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return allVersions;
+    }
 }
